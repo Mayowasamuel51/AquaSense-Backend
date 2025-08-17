7394169a16b8b7dd03f4b54a1b654a22fc701940
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-08-17 16:23:17 +0100

    adding to my backend

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 # from config import DATABASE_URL
-DATABASE_URL = "mysql+pymysql://apimypr5_mypromosphere:mypromosphere@131.153.147.186:3306/apimypr5_mypromosphere"
+DATABASE_URL = "mysql+pymysql://apimypr5_mypromosphere:mypromosphere@131.153.147.186:3306/apimypr5_AquaSenseBackend"
+
 # DATABASE_URL = "mysql+pymysql://apimypr5_mypromosphere:mypromosphere@db.yourhost.com:3306/fastapi_db"
 # DATABASE_URL = "mysql+pymysql://apimypr5_mypromosphere:mypromosphere@apimypr5_AquaSenseBackend:3306/fastapi_db"
 engine = create_engine(DATABASE_URL)
