0dc0b1cdca0d08a1704eced55e75af1fcdcc42b1
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-08-17 16:17:17 +0100

    DATABASE_URL = mysql+pymysql://apimypr5_mypromosphere:mypromosphere@131.153.147.186:3306/apimypr5_mypromosphere

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# from config import DATABASE_URL
+DATABASE_URL = "mysql+pymysql://apimypr5_mypromosphere:mypromosphere@131.153.147.186:3306/apimypr5_mypromosphere"
+# DATABASE_URL = "mysql+pymysql://apimypr5_mypromosphere:mypromosphere@db.yourhost.com:3306/fastapi_db"
+# DATABASE_URL = "mysql+pymysql://apimypr5_mypromosphere:mypromosphere@apimypr5_AquaSenseBackend:3306/fastapi_db"
+engine = create_engine(DATABASE_URL)
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
