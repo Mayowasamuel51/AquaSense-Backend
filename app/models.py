d86155619f4edece018f26e67eb2f8a4940c7bfc
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-09-17 20:13:10 +0100

    newnw

--- app/models.py
+++ app/models.py
@@ -73,7 +73,7 @@ class VerificationToken(Base):
     id = Column(Integer,primary_key=True, index=True, autoincrement=True)
     token = Column(String(255), unique=True, index=True, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id"))
-    expires_at = Column(DateTime, default=lambda: datetime.datetime.utcnow() + datetime.timedelta(hours=1))
+    expires_at = Column(DateTime, default=lambda: datetime.datetime.utcnow() + datetime.timedelta(hours=2))
     user = relationship("User", back_populates="tokens")
 
 class Worker(Base):
