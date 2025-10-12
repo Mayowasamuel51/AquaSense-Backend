fde00c129119e398bf419beb3d5f45af326769cf
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-10-12 10:49:57 +0100

    chekcin g

--- app/models.py
+++ app/models.py
@@ -185,7 +185,7 @@ class VerificationToken(Base):
     id = Column(Integer,primary_key=True, index=True, autoincrement=True)
     token = Column(String(255), unique=True, index=True, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id"))
-    vendor_id = Column(Integer, ForeignKey("vendor.id"))
+    vendor_id = Column(Integer, ForeignKey("vendor.id"), nullable=True)
     # expires_at = Column(DateTime, default=lambda: datetime.datetime.utcnow() + datetime.timedelta(hours=2))
     user = relationship("User", back_populates="tokens")
     # relationship back to vendor
