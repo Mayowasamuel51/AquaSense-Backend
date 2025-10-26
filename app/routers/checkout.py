437d977ca07c172aa5c793f5dca84d611305b227
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-10-26 18:22:08 +0100

    making code farmer_info

--- app/routers/checkout.py
+++ app/routers/checkout.py
@@ -366,13 +366,14 @@ def paystack_callback(reference: str, request: Request, db: Session = Depends(ge
     delivery_info = None
     if delivery:
         delivery_info = {
-            "recipient_name": delivery.recipient_name,
-            "phone_number": delivery.phone_number,
-            "address": delivery.address,
-            "city": delivery.city,
-            "state": delivery.state,
-            "postal_code": delivery.postal_code,
-            "delivery_instructions": delivery.delivery_instructions,
+            "first_name": delivery_address.first_name,
+            "last_name": delivery_address.last_name,
+            "delivery_address": delivery_address.delivery_address,
+            "phone_number": delivery_address.phone_number,
+            "additional_number": delivery_address.additional_number,
+            "city": delivery_address.city,
+            "state": delivery_address.state,
+            "postal_code": delivery_address.postal_code,
         }
 
     # ✅ Prepare full JSON response
